4cfe8cab586d30955182a306f30f0536f6fb3e1a
Logan Weber <weberlo@cs.washington.edu>
2021-12-11 20:35:42 -0500

    Impl contravariant hom functor

--- hom_functor.py
+++ hom_functor.py
@@ -2,15 +2,29 @@ from category import *
 from functor import *
 from cat_insts import *
 
-def build_covariant_hom_functor(C: Category, X: 'Object[C]', SC: SetCat):
-    def F_obj(Y: 'Object[C]'):
-        return SC.find(C.hom[(X, Y)])
+def build_covariant_hom_functor(C: Category, A: 'Object[C]', SC: SetCat):
+    def F_obj(X: 'Object[C]'):
+        return SC.find(C.hom[(A, X)])
 
-    def F_mor(f: 'Morphism[Y, Z]'):
+    def F_mor(f: 'Morphism[X, Y]'):
         res = SetMorSym(lambda g: g >> f, f'· >> {f.sym}', f)
         F_src = F_obj(f.src)
         F_tgt = F_obj(f.tgt)
         return SC.find_mor(Morphism(F_src, res, F_tgt, is_ident=f.is_ident))
+
+    return Functor(C, SC, F_obj, F_mor)
+
+
+def build_contravariant_hom_functor(C: Category, B: 'Object[C]', SC: SetCat):
+    def F_obj(X: 'Object[C]'):
+        return SC.find(C.hom[(X, B)])
+
+    def F_mor(f: 'Morphism[X, Y]'):
+        res = SetMorSym(lambda g: f >> g, f'{f.sym} >> ·', f)
+        F_src = F_obj(f.src)
+        F_tgt = F_obj(f.tgt)
+        # Note we've flipped the src and tgt.
+        return SC.find_mor(Morphism(F_tgt, res, F_src, is_ident=f.is_ident))
 
     return Functor(C, SC, F_obj, F_mor)
 
--- test_hom_functor.py
+++ test_hom_functor.py
@@ -1,19 +1,32 @@
 from hom_functor import *
 from cat_insts import *
 
+WA = build_walking_arrow()
+one = WA.find('1')
+two = WA.find('2')
+f = WA.find('f')
+id_1 = WA.find('id_1')
+id_2 = WA.find('id_2')
 
-Two = build_walking_arrow()
-one = Two.find('1')
-two = Two.find('2')
-f = Two.find('f')
-id_1 = Two.find('id_1')
-id_2 = Two.find('id_2')
+def test_covariant():
+    SC = SetCat()
+    WA_1_x = build_covariant_hom_functor(WA, one, SC)
+    print(WA_1_x(one))
+    print(WA_1_x(two))
+    print(WA_1_x(f))
+    print(WA_1_x(id_1))
+    print(WA_1_x(id_2))
 
-SC = SetCat()
 
-Two_1 = build_covariant_hom_functor(Two, one, SC)
-print(Two_1(one))
-print(Two_1(two))
-print(Two_1(f))
-print(Two_1(id_1))
-print(Two_1(id_2))
+def test_contravariant():
+    SC = SetCat()
+    WA_x_1 = build_contravariant_hom_functor(WA, one, SC)
+    print(WA_x_1(one))
+    print(WA_x_1(two))
+    print(WA_x_1(f))
+    print(WA_x_1(id_1))
+    print(WA_x_1(id_2))
+
+
+if __name__ == '__main__':
+    test_contravariant()
