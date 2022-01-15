1a77b87fdd114a6dae5420ff9aa1ea70b8aa89e4
Logan Weber <weberlo@cs.washington.edu>
2022-01-15 17:00:24 -0500

    Simplify extensional group definition

--- scratchpad.py
+++ scratchpad.py
@@ -7,19 +7,18 @@
 e = lambda x: x
 elts = [e]
 
-def a(x):
-    if x == e:
-        return a
-    elif x == a:
-        return e
-    elif eq(x, e, elts):
-        print('here')
-        return a
-    elif eq(x, a, elts):
-        print('heree')
-        return e
-    assert False
+table = {
+    (e, e): e
+}
 
+
+def a(x):
+    x_base = find_base(x, elts)
+    return table[(a, x_base)]
+
+table[(a, a)] = e
+table[(a, e)] = a
+table[(e, a)] = a
 
 # We close the loop here!
 elts.append(a)
@@ -39,10 +38,14 @@ def comp_rule(f, g, elts):
     return res
 
 
-# def find_base(x, S):
-#     if x in S:
-#         return x
-#     return find_base(graph_to_elt[graph(x, S)], S)
+def find_base(x, S):
+    if x in S:
+        return x
+    x_graph = graph(x, S)
+    res = find_base(graph_to_elt[x_graph], S)
+    # Path compression
+    graph_to_elt[x_graph] = res
+    return res
 
 
 def graph(x, S):
@@ -53,16 +56,15 @@ def eq(x, y, S):
     return graph(x, S) == graph(y, S)
 
 
-# # Add all of the base elements to the graph-to-element cache.
-# for elt in elts:
-#     graph_to_elt[graph(elt, elts)] = elt
+# Add all of the base elements to the graph-to-element cache.
+for elt in elts:
+    graph_to_elt[graph(elt, elts)] = elt
 
 
 aa = comp_rule(a, a, elts)
 aaaa = comp_rule(aa, aa, elts)
 ea = comp_rule(e, a, elts)
 
-print(f'{elts=}')
 print(f'{graph(e, elts)=}')
 print(f'{graph(a, elts)=}')
 print(f'{graph(aa, elts)=}')
@@ -74,3 +76,6 @@ print(f'{eq(ea, e(a), elts)=}')
 print(f'{eq(a, a(e), elts)=}')
 print(f'{eq(a, a(aa), elts)=}')
 print(f'{eq(e, aa(aa), elts)=}')
+print(f'{eq(e, a, elts)=}')
+print(f'{eq(a, e, elts)=}')
+print(f'{eq(a(e), e(a), elts)=}')
